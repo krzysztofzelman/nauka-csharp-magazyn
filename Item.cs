208b3df9b5e424dec2c0b2a4466a0a709e7e5a8f
krzysztofzelman666-png <krzysztofzelman666@gmail.com>
2026-07-27 21:09:56 +0200

    Pierwszy commit - klasa Item i menu

---
+++ Item.cs
@@ -0,0 +1,15 @@
+class Item
+{
+    public string Name;
+    public int Quantity;
+    public decimal Price;
+
+    public Item(string name, int quantity, decimal price)
+    {
+     Name = name;   
+     Quantity = quantity;
+     Price = price;
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿List<Item> magazyn = new List<Item>();
+while (true)
+{
+
+    Console.WriteLine("📦 Witaj w systemie magazynowym!");
+    Console.WriteLine("Wybierz opcje:");
+    Console.WriteLine("1 - Dodaj przedmiot");
+    Console.WriteLine("2 - Wyświetl wszystkie");
+    Console.WriteLine("3 - Wyjście");
+    string wybor = Console.ReadLine() ?? "";
+
+    if (wybor == "1")
+    {
+        Console.WriteLine("Podaj nazwę przedmiotu:)");
+        String nazwa = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Podaj ilość");
+        string iloscTekst = Console.ReadLine() ?? "";
+        int ilosc = int.Parse(iloscTekst);
+
+        Console.WriteLine("Podaj cenę za sztukę");
+        string cenaTekst = Console.ReadLine() ?? "";
+        decimal cena = decimal.Parse(cenaTekst);
+        magazyn.Add(new Item(nazwa,ilosc,cena));
+    }
+    else if (wybor == "2")
+        if (magazyn.Count == 0)
+        {
+            Console.WriteLine("📭 Magazyn jest pusty.");
+        }
+        else
+        {
+            Console.WriteLine("=== Zawartość magazynu ===");
+            foreach (Item przedmiot in magazyn)
+            {
+                Console.WriteLine($"-{przedmiot.Name} | {przedmiot.Quantity} szt. | {przedmiot.Price} zł");
+            }
+        }
+    else if (wybor == "3")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("❌ Nie ma takiej opcji!");
+    }
+
+}
